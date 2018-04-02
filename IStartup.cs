01ccf62f152ba2411abcbb398b7c7bc08a10cf9c
dx Team <32604377+dx-team@users.noreply.github.com>
2018-04-02 02:56:34 -0300

    Initial commit

---
+++ IStartup.cs
@@ -0,0 +1,17 @@
+﻿#if FW
+using Owin;
+#elif NETCORE
+using Microsoft.AspNetCore.Builder; 
+#endif
+
+namespace DxLibs.Web
+{
+    public interface IStartup
+    {
+#if FW
+        void Configure(IAppBuilder app);
+#elif NETCORE
+        void Configure(IApplicationBuilder app);
+#endif
+    }
+}
